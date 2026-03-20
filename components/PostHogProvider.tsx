2a826941767d0df7def1a0e52d8fdf899112fa7b
T-tsuu <laabassi.moustafa7@gmail.com>
2026-03-20 22:17:33 +0100

    Implemeting Posthog

--- components/PostHogProvider.tsx
+++ components/PostHogProvider.tsx
@@ -35,7 +35,8 @@ function SuspendedPostHogPageView() {
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      api_host: '/ingest',
+      ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
       capture_pageview: false,
       capture_pageleave: true,
     })
